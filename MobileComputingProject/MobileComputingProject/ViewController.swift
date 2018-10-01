1d647c64231d2a88e8329774e49aaabb5d41cebc
Daniel Roeven <daniel@roeven.com>
2018-10-01 10:34:16 +0200

    Fixed deleting Pois when zoomed in

--- MobileComputingProject/MobileComputingProject/ViewController.swift
+++ MobileComputingProject/MobileComputingProject/ViewController.swift
@@ -126,7 +126,10 @@ class ViewController: UIViewController, UIScrollViewDelegate, CLLocationManagerD
     var movePoiOffsetX:CGFloat = 0
     var movePoiOffsetY:CGFloat = 0
     @objc func poiTapped(gesture: UILongPressGestureRecognizer) {
-        // On LongPress begin, give haptic feedback (vibration) and save offset
+        // On LongPress begin
+        // Give haptic feedback (vibration)
+        // Save offset
+        // Show trash icon
         if gesture.state == .began {
             UIImpactFeedbackGenerator.init(style: UIImpactFeedbackStyle.heavy).impactOccurred()
             movePoiOffsetX = gesture.location(in: gesture.view).x
@@ -147,8 +150,11 @@ class ViewController: UIViewController, UIScrollViewDelegate, CLLocationManagerD
             
             view!.frame = CGRect(x: newX, y: newY, width: 48, height: 48)
         }
+        // If LongPress ended
+        // And Poi dragged to trash
+        // Delete Poi, hide trash
         else if gesture.state == .ended {
-            if (trashCanView.frame.intersects(gesture.view!.frame)) {
+            if (trashCanView.frame.intersects(view.convert(gesture.view!.frame, from: floorPlanView))) {
                 // FIXME: Check if user has right to delete Poi
                 gesture.view!.removeFromSuperview()
                 // FIXME: Send delete request to firebase when deleting Poi
