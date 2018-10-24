bd6197e92d5bf9e3851e293909fab6f4193567d5
SimonTakman <simontakman@gmail.com>
2018-10-24 11:37:27 +0200

    Deleted DetailedViewShape and refactored OnBoardingController

--- MobileComputingProject/MobileComputingProject/OnBoardingController.swift
+++ MobileComputingProject/MobileComputingProject/OnBoardingController.swift
@@ -15,17 +15,12 @@ class OnBoardingController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         animationView.frame = CGRect(x: 0, y: 0, width: 300, height: 300)
         animationView.center = self.view.center
         animationView.contentMode = .scaleAspectFill
-        
         self.view.addSubview(animationView)
-        
         animationView.loopAnimation = true
-        
         animationView.play()
-
         // Do any additional setup after loading the view.
     }
     
@@ -36,15 +31,4 @@ class OnBoardingController: UIViewController {
     @objc func enterFirstTime() {
         self.performSegue(withIdentifier: "enterFirstTime", sender: self)
     }
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
